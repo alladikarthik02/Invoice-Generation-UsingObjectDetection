f41434fcf2dc00612ab7ea5404f80fa8cdd2ab09
Karthik Alladi <karthikalladi@Karthiks-MacBook-Pro.local>
2023-06-06 10:31:51 +0530

    Server changed

--- ML/Server/server_main.py
+++ ML/Server/server_main.py
@@ -1,54 +1,58 @@
 from flask import Flask, Response, render_template, request, jsonify, url_for, redirect
-#from flask import current_app as app
 from flask_cors import CORS, cross_origin
 import cv2
 import base64
 from ultralytics import YOLO
 import time
 
-app = Flask(__name__,template_folder='../template')
+app = Flask(__name__, template_folder='../template')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-#app.app_context().push()
+# app.app_context().push()
 
-items = {0: 'CloseUp', 1: 'Cocoa Powder', 2: 'Colgate', 3: 'Hershey-s', 4: 'KeraGlo', 5: 'Lays', 6: 'Loreal', 7: 'Maggi', 8: 'MarieLight', 9: 'Perk'}
+items = {0: 'CloseUp', 1: 'Cocoa Powder', 2: 'Colgate', 3: 'Hershey-s',
+         4: 'KeraGlo', 5: 'Lays', 6: 'Loreal', 7: 'Maggi', 8: 'MarieLight', 9: 'Perk'}
 
-id_dict = {'CloseUp': 'hdsbi78dfY', 'Cocoa Powder': 'kahv238923', 'Maggi': 'jhdvsDjh3f', 'Hershey-s': 'kjbw23jhvh', 'KeraGlo': 'JHVgcYVj67', 'Lays': 'Ftuc88cUTI', 'Loreal': 'hvIViV89yv', 'Maggi': 'UC8u8cY8vi', 'MarieLight': 'iyv9779v97', 'Perk': 'iyvI9v9V76'}
+id_dict = {'CloseUp': 'hdsbi78dfY', 'Cocoa Powder': 'kahv238923', 'Maggi': 'jhdvsDjh3f', 'Hershey-s': 'kjbw23jhvh', 'KeraGlo': 'JHVgcYVj67',
+           'Lays': 'Ftuc88cUTI', 'Loreal': 'hvIViV89yv', 'MarieLight': 'iyv9779v97', 'Perk': 'iyvI9v9V76', 'Colgate': 'etyd7890we', 'None': 'None'}
 
-model=YOLO("C:\\Users\\Harsha\\retail-billing-system\\runs\\detect\\train\\weights\\best.pt")
+model = YOLO(
+    "/Users/karthikalladi/VSCODES/Backend/train-desi/weights/best.pt")
+
 
 def gen_frames():
+    flag = 0
     detected_prod = 'None'
     cap = cv2.VideoCapture(0)
-    while True:
+    start = time.time()
+    end = time.time()
+    while (end-start) <= 7 or flag == 1:
         success, frame = cap.read()
         if success:
-            results = model(frame, show=False, conf=0.85, hide_labels=True)
-            annotated_frame = results[0].plot(show_conf=False, line_thickness=10)
+            results = model(frame, show=False, conf=0.85, show_labels=True)
+            annotated_frame = results[0].plot(
+                conf=False, line_width=10)
             product = results[0].boxes.cls.tolist()
             if product:
-                #app.test_client().post('http://localhost:5000/product-name', data=items[product[0]])
-                #time.sleep(3)
-                #detected_prod = str(items[product[0]])
+                flag = 1
                 return items[product[0]]
-                #yield redirect('http://localhost:3000/')
             annotated_frame = cv2.resize(annotated_frame, (780, 400))
             ret, buffer = cv2.imencode('.jpg', annotated_frame)
-            #annotated_frame = buffer.tobytes()
-            #encoded_image = (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + annotated_frame + b'\r\n')
-            #frame_data = base64.b64encode(buffer).decode('utf-8')
-            #product_message = {'detected_product': detected_prod}
-            #yield jsonify({'product': product_message,'frame': frame_data})
+            end = time.time()
         else:
             cap.release()
             cv2.destroyAllWindows()
+    if (flag == 0):
+        return detected_prod
+
 
 def annotate():
     while True:
         frame = request.files
         if frame:
             results = model(frame, show=False, conf=0.85, hide_labels=True)
-            annotated_frame = results[0].plot(show_conf=False, line_thickness=10)
+            annotated_frame = results[0].plot(
+                show_conf=False, line_thickness=10)
             product = results[0].boxes.cls.tolist()
             if product:
                 time.sleep(1)
@@ -59,12 +63,13 @@ def annotate():
             flag, buffer = cv2.imencode('.jpg', annotated_frame)
             annotated_frame = buffer.tobytes()
             return (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + annotated_frame + b'\r\n')
-        
+
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def index():
     return render_template('home.html')
+
 
 @app.route("/video-feed", methods=['GET'])
 @cross_origin()
@@ -72,14 +77,16 @@ def video_feed():
     result = gen_frames()
     id = id_dict[result]
     if result:
-        return redirect('http://localhost:3000/'+id)
-    #return Response(gen_frames(), mimetype='application/json')
+        return redirect('http://localhost:3000/add-product/'+id)
+    # return Response(gen_frames(), mimetype='application/json')
+
 
 @app.route("/process-frames", methods=['POST'])
 @cross_origin()
 def process_frames():
     return Response(annotate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == "__main__":
-    #with app.app_context():
-        app.run(debug=True)
+    # with app.app_context():
+    app.run(debug=True)
